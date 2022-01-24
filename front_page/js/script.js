d74eaf0ca9c86088c26b7485d27606ca98a760a9
kei.sakanishi@gmail.com <kei.sakanishi@gmail.com>
2022-01-24 17:47:14 +0900

    残り:mapの可変,画像切り替え,fa,アニメーション,PC版

--- front_page/js/script.js
+++ front_page/js/script.js
@@ -2,18 +2,29 @@
 
 const btn = document.querySelector('.js-btn');
 const menu = document.querySelector('.js-menu');
-const closeBtn = document.querySelector('.js-closeBtn')
+const closeBtn = document.querySelector('.js-closeBtn');
+const openSidebar = () => {
+    menu.classList.add('is-open')
+    console.log('OpenSidebar')
+};
+const closeSidebar = () => {
+    menu.classList.remove('is-open')
+    console.log('CloseSidebar')
+    console.log
+};
 console.log(btn);
 console.log(menu);
 console.log(closeBtn);
 
 btn.addEventListener('click', () => {
-    menu.classList.add('is-open')
-    console.log('OpenSidebar')
+    // menu.classList.add('is-open')
+    // console.log('OpenSidebar')
+    openSidebar();
 });
 
 closeBtn.addEventListener('click', () =>{
-    menu.classList.remove('is-open')
-    console.log('CloseSidebar')
+    // menu.classList.remove('is-open')
+    // console.log('CloseSidebar')
+    closeSidebar();
 });
 
