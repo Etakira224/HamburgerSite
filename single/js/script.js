db3eca95ebc4b0bcfe1eb53f9f9690a9ad7b0f61
kei.sakanishi@gmail.com <kei.sakanishi@gmail.com>
2022-02-15 18:35:51 +0900

    HTML

---
+++ single/js/script.js
@@ -0,0 +1,29 @@
+/* https://www.webcreatorbox.com/tech/slide-menu */
+
+const btn = document.querySelector('.js-btn');
+const menu = document.querySelector('.js-menu');
+const closeBtn = document.querySelector('.js-closeBtn');
+const openSidebar = () => {
+    menu.classList.add('is-open')
+    console.log('OpenSidebar')
+};
+const closeSidebar = () => {
+    menu.classList.remove('is-open')
+    console.log('CloseSidebar')
+};
+console.log(btn);
+console.log(menu);
+console.log(closeBtn);
+
+btn.addEventListener('click', () => {
+    // menu.classList.add('is-open')
+    // console.log('OpenSidebar')
+    openSidebar();
+});
+
+closeBtn.addEventListener('click', () =>{
+    // menu.classList.remove('is-open')
+    // console.log('CloseSidebar')
+    closeSidebar();
+});
+
